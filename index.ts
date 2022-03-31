ea1e1210d29201e03113419d758398ab7322f6a1
James Camilleri <james@james.mt>
2022-04-01 00:41:16 +0100

    Configure ngrok and heartbeat

--- index.ts
+++ index.ts
@@ -1,8 +1,7 @@
-import { dirname, join } from 'path'
-import { fileURLToPath } from 'url'
-
 import { DMX, EnttecUSBDMXProDriver, NullDriver } from 'dmx-ts'
 import express from 'express'
+import ngrok from 'ngrok'
+import fetch from 'node-fetch'
 
 import { queueDmxUpdates } from './queue-commands/index.js'
 import { textToDmx } from './text-to-dmx/index.js'
@@ -10,25 +9,41 @@ import { textToDmx } from './text-to-dmx/index.js'
 const DEBUG_UNIVERSE = true
 const USB_PORT = 'COM3'
 const NETWORK_PORT = 1992
+const CONFIG_URL = 'https://typo.digital/config'
 
 function getDriver() {
   if (DEBUG_UNIVERSE) return new NullDriver()
   return new EnttecUSBDMXProDriver(USB_PORT)
 }
 
+async function connect() {
+  const url = await ngrok.connect(NETWORK_PORT)
+  await fetch(`${CONFIG_URL}/ngrok`, {
+    method: 'POST',
+    body: JSON.stringify({ url }),
+  })
+}
+
 async function main() {
+  await connect()
+
   const dmx = new DMX()
   const typewriterUniverse = await dmx.addUniverse('typewriter', getDriver())
 
   const app = express().use(express.json())
-  app.get('/', (req, res) => {
-    res.sendFile(join(dirname(fileURLToPath(import.meta.url)), 'ui/index.html'))
+
+  app.get('/heartbeat', (req, res) => {
+    res.send('OK')
   })
 
   app.post('/', (req, res) => {
-    const { text } = req.body
-    const channelData = textToDmx(text)
-    queueDmxUpdates(typewriterUniverse, channelData)
+    try {
+      const channelData = textToDmx(req.body.text)
+      queueDmxUpdates(typewriterUniverse, channelData)
+    } catch (e) {
+      res.status(500).send(e)
+      return
+    }
 
     res.send('OK')
   })
