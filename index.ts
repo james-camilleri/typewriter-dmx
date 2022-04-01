85cdbdb20983216c948286cb2b7210f99f5b4281
James Camilleri <james@james.mt>
2022-04-01 18:10:23 +0100

    Add basic logging

--- index.ts
+++ index.ts
@@ -3,12 +3,14 @@ import express from 'express'
 import ngrok from 'ngrok'
 import fetch from 'node-fetch'
 
+import { log } from './log/index.js'
 import { queueDmxUpdates } from './queue-commands/index.js'
 import { textToDmx } from './text-to-dmx/index.js'
 
 const DEBUG_UNIVERSE = false
 const USB_PORT_WINDOWS = 'COM3'
-const USB_PORT_PI = '/dev/serial/by-id/usb-ENTTEC_DMX_USB_PRO_EN236685-if00-port0'
+const USB_PORT_PI =
+  '/dev/serial/by-id/usb-ENTTEC_DMX_USB_PRO_EN236685-if00-port0'
 const NETWORK_PORT = 1992
 const CONFIG_URL = 'https://typo.digital/config'
 
@@ -18,14 +20,22 @@ function getDriver() {
 }
 
 async function connect() {
+  log.info('Creating ngrok tunnel')
   const url = await ngrok.connect(NETWORK_PORT)
+
+  log.info('Pushing tunnel URL to TypOnline')
   await fetch(`${CONFIG_URL}/ngrok`, {
     method: 'POST',
     body: JSON.stringify({ url }),
   })
 }
 
+async function newline() {}
+
 async function main() {
+  log.info('Initialising typewriter-dmx')
+  log.info(`Debug mode ${DEBUG_UNIVERSE ? 'enabled' : 'disabled'}`)
+
   await connect()
 
   const dmx = new DMX()
@@ -39,7 +49,10 @@ async function main() {
 
   app.post('/', (req, res) => {
     try {
+      const { text } = req.body
+      log.info('Received text:', text)
       const channelData = textToDmx(req.body.text)
+      log.info('Converted to channel data:', channelData)
       queueDmxUpdates(typewriterUniverse, channelData)
     } catch (e) {
       res.status(500).send(e)
@@ -50,6 +63,7 @@ async function main() {
   })
 
   app.listen(NETWORK_PORT)
+  log.info(`Listening on port ${NETWORK_PORT}`)
 }
 
 main().catch(console.error)
---
+++ log/index.ts
@@ -0,0 +1,16 @@
+const LOG_LEVEL = {
+  ERROR: 'ERROR',
+  WARNING: 'WARNING',
+  INFO: 'INFO',
+} as const
+type LogLevel = typeof LOG_LEVEL[keyof typeof LOG_LEVEL]
+
+function _log(level: LogLevel, ...args: any[]) {
+  console.log(`[${level}] `, ...args)
+}
+
+export const log = {
+  error: (...args: any[]) => _log(LOG_LEVEL.ERROR, ...args),
+  warning: (...args: any[]) => _log(LOG_LEVEL.WARNING, ...args),
+  info: (...args: any[]) => _log(LOG_LEVEL.INFO, ...args),
+}
