4af93b03edaac2f1343bd7972e0f15781e55e605
James Camilleri <james@james.mt>
2022-05-03 20:44:10 +0100

    Adjust Pi GPIO config

--- queue/handlers/motor.ts
+++ queue/handlers/motor.ts
@@ -3,9 +3,9 @@ import { DigitalOutput, HIGH, LOW, PULL_DOWN } from 'raspi-gpio'
 
 import { Config } from '../../types'
 
-const PULSE_PIN = 17
-const DIRECTION_PIN = 27
-const ENABLE_PIN = 22
+const PULSE_PIN = 'GPIO17'
+const DIRECTION_PIN = 'GPIO27'
+const ENABLE_PIN = 'GPIO22'
 
 export function createMotorCommandHandler(config: Config) {
   const { charsPerLine, newlineRotationDegrees } = config
@@ -15,12 +15,10 @@ export function createMotorCommandHandler(config: Config) {
       init(() => {
         const pullResistor = PULL_DOWN
 
-        const pulse = new DigitalOutput({ pin: PULSE_PIN, pullResistor })
-        const enable = new DigitalOutput({ pin: ENABLE_PIN, pullResistor })
+        const pulse = new DigitalOutput({ pin: PULSE_PIN })
+        const enable = new DigitalOutput({ pin: ENABLE_PIN })
         const direction = new DigitalOutput({
-          pin: DIRECTION_PIN,
-          pullResistor,
-        })
+          pin: DIRECTION_PIN })
 
         pulse.write(HIGH)
         enable.write(HIGH)
