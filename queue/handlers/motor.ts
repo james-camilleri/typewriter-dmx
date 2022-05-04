167d3d67250293b95ad8ca4e640a277a5f555e6e
James Camilleri <james@james.mt>
2022-05-04 14:18:36 +0100

    Clean up motor code

--- queue/handlers/motor.ts
+++ queue/handlers/motor.ts
@@ -14,33 +14,36 @@ const CONTROL_2 = new Gpio(CONTROL_PIN_2, { mode: Gpio.OUTPUT })
 const ENABLE = new Gpio(ENABLE_PIN, { mode: Gpio.OUTPUT })
 
 CONTROL_1.digitalWrite(LOW)
-CONTROL_1.digitalWrite(LOW)
+CONTROL_2.digitalWrite(LOW)
 ENABLE.digitalWrite(LOW)
 
 function getLoopParams(steps: number | string) {
   const repetitions = typeof steps === 'number' ? steps : Number(steps)
   const by1 = repetitions % 256
   const by256 = Math.floor(repetitions / 256)
-  
-  console.log('loops', [by1, by256])
+
   return [by1, by256]
+}
+
+interface MotorData {
+  steps: number
+  speed: 'slow' | 'fast'
 }
 
 export function createMotorCommandHandler(config: Config) {
   const { charsPerLine, newlineRotationDegrees, slowDelay, fastDelay } = config
-  
+
   const MICROSECOND_DELAY_SLOW = slowDelay ?? 500
   const MICROSECOND_DELAY_FAST = fastDelay ?? 50
 
-  return async ({ steps, speed = 'slow' }: { steps: number, speed: 'slow' | 'fast' }) => {
+  return async ({ steps, speed = 'slow' }: MotorData) => {
     return new Promise<void>((resolve, reject) => {
       pigpio.waveClear()
       ENABLE.digitalWrite(HIGH)
-      
-      const usDelay = speed === 'slow'
-        ? MICROSECOND_DELAY_SLOW
-        : MICROSECOND_DELAY_FAST
-        
+
+      const usDelay =
+        speed === 'slow' ? MICROSECOND_DELAY_SLOW : MICROSECOND_DELAY_FAST
+
       const clockwise = steps > 0
       const pin1 = clockwise ? CONTROL_PIN_1 : CONTROL_PIN_2
       const pin2 = clockwise ? CONTROL_PIN_2 : CONTROL_PIN_1
@@ -64,23 +67,17 @@ export function createMotorCommandHandler(config: Config) {
       if (waveId >= 0) {
         // Loop wave for `steps` amount.
         // See https://github.com/fivdi/pigpio/blob/master/doc/global.md#wavechainchain
-        pigpio.waveChain([
-          255, 0,
-          waveId,
-          255, 1, ...getLoopParams(steps)
-        ])
+        pigpio.waveChain([255, 0, waveId, 255, 1, ...getLoopParams(steps)])
       }
-      
-      // TODO: figure out how to detect wave end.
-      console.log('length in micros', pigpio.waveGetMicros())
-      const length = pigpio.waveGetMicros() * Number(steps)
-      console.log('full length in micros', length)
-      console.log('length in seconds', length / 1000 / 1000)
-      
-      const timeoutLength = (length / 1000) * 2 + 1500
+
+      // TODO: Remove the number cast later.
+      const lengthMicroseconds = pigpio.waveGetMicros() * Number(steps)
+
+      // Add 1.5s safety gap.
+      // I have no idea why it's currently taking twice as long as expected.
+      const timeoutLength = (lengthMicroseconds / 1000) * 2 + 1500
 
       setTimeout(() => {
-        // STAHP.
         ENABLE.digitalWrite(LOW)
         pigpio.waveDelete(waveId)
         resolve()
