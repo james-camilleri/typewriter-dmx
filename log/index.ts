890d297fc300554db2c35730ef8ee4fc9c9d0e14
James Camilleri <j.camilleri@cmcmarkets.com>
2022-08-25 15:21:31 +0100

    Fire log events for client-side monitoring

--- log/index.ts
+++ log/index.ts
@@ -1,3 +1,5 @@
+import { emitter } from '../events'
+
 const LOG_LEVEL = {
   ERROR: 'ERROR',
   WARNING: 'WARNING',
@@ -7,6 +9,7 @@ type LogLevel = typeof LOG_LEVEL[keyof typeof LOG_LEVEL]
 
 function _log(level: LogLevel, ...args: any[]) {
   console.log(`[${level}] `, ...args)
+  emitter.fireEvent('log', `[${level}] ${args.join(' ')}`)
 }
 
 export const log = {
