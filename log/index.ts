78e43b82c79bed73424fdd9b0474f6e993042bc2
James Camilleri <james@james.mt>
2022-08-31 00:09:19 +0100

    Reduce cached logging

--- log/index.ts
+++ log/index.ts
@@ -4,10 +4,11 @@ const LOG_LEVEL = {
   ERROR: 'ERROR',
   WARNING: 'WARNING',
   INFO: 'INFO',
+  DEBUG: 'DEBUG',
 } as const
 type LogLevel = typeof LOG_LEVEL[keyof typeof LOG_LEVEL]
 
-const cache = []
+let cache: string[] = []
 
 function _log(level: LogLevel, ...args: any[]) {
   const log = `[${level}] ${args.join(' ')}`
@@ -18,11 +19,29 @@ function _log(level: LogLevel, ...args: any[]) {
 }
 
 export function flushCache() {
-  cache.forEach((log) => emitter.fireEvent('log', log))
+  emitter.fireEvent(
+    'log',
+    '[DEBUG] Flushing cache ------------------------------',
+  )
+
+  cache.forEach((log) =>
+    emitter.fireEvent(
+      'log',
+      log.replace(/\[(ERROR|WARNING|INFO)\]/, `[${LOG_LEVEL.DEBUG}]`),
+    ),
+  )
+
+  emitter.fireEvent(
+    'log',
+    '[DEBUG] Flushed cache ------------------------------',
+  )
+
+  cache = []
 }
 
 export const log = {
   error: (...args: any[]) => _log(LOG_LEVEL.ERROR, ...args),
   warning: (...args: any[]) => _log(LOG_LEVEL.WARNING, ...args),
   info: (...args: any[]) => _log(LOG_LEVEL.INFO, ...args),
+  debug: (...args: any[]) => _log(LOG_LEVEL.DEBUG, ...args),
 }
